81981ff7caaad340a33a4f61f3c21b98f810de41
Heribert Schütz <hs@webxcerpt.com>
2022-03-30 14:55:53 +0200

    provide an "all versions" checkbox in the UI

--- in-browser-test/src/utils.tsx
+++ in-browser-test/src/utils.tsx
@@ -1,26 +1,53 @@
-import { FC } from "react";
+import { FC, MutableRefObject, useEffect, useRef } from "react";
 import styled from "styled-components";
 import { versions } from "./versions";
+
+const versionNames = Object.keys(versions);
 
 export const SelectVersions: FC<{
   selected: Record<string, boolean>,
   setSelected: (v: Record<string, boolean>) => void,
-}> = ({selected, setSelected}) => (
-  <div style={{display: "flex", flexFlow: "row wrap"}}>
-    {Object.keys(versions).map(name => (
-      <label key={name} style={{flexBasis: "4em", whiteSpace: "nowrap"}}>
+}> = ({selected, setSelected}) => {
+  const allRef: MutableRefObject<HTMLInputElement | null> = useRef(null);
+  useEffect(() => {
+    const all = allRef.current;
+    if (all) {
+      all.checked =
+        versionNames.every(name => selected[name]);
+      all.indeterminate =
+        versionNames.some(name => selected[name]) &&
+        versionNames.some(name => !selected[name]);
+    }
+  }, [selected]);
+  return (
+    <div>
+      <label style={{flexBasis: "4em", whiteSpace: "nowrap"}}>
         <input type="checkbox"
-          checked={Boolean(selected[name])}
-          onChange={event => setSelected({
-            ...selected,
-            [name]: event.target.checked
-          })}
+          ref={allRef}
+          onChange={event => {
+            const checked = event.target.checked;
+            setSelected(Object.fromEntries(versionNames.map(name => [name, checked])));
+          }}
         />
-        {name}
+        all versions
       </label>
-    ))}
-  </div>
-)
+      <div style={{display: "flex", flexFlow: "row wrap"}}>
+        {versionNames.map(name => (
+          <label key={name} style={{flexBasis: "4em", whiteSpace: "nowrap"}}>
+            <input type="checkbox"
+              checked={Boolean(selected[name])}
+              onChange={event => setSelected({
+                ...selected,
+                [name]: event.target.checked
+              })}
+            />
+            {name}
+          </label>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 
 export const Table = styled.table`
