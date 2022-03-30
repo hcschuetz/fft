97ae8e3fefd711028bc867abac4be2c26b695f06
Heribert Schütz <hs@webxcerpt.com>
2022-03-30 18:26:15 +0200

    UI: Test results are displayed as a square table
    ... with each field showing the difference between two FFT versions.
    We no more care about which other versions a version is based on.
    
    The difference between IFFT(FFT(input)) and input is still displayed.

--- in-browser-test/src/testing.tsx
+++ in-browser-test/src/testing.tsx
@@ -1,15 +1,17 @@
 import { abs2, Complex, minus, timesScalar } from "complex/dst/Complex";
-import { getComplex, makeComplexArray } from "complex/dst/ComplexArray";
+import { ComplexArray, getComplex, makeComplexArray, setComplex } from "complex/dst/ComplexArray";
 import { makeTestData } from "./test-utils";
 import { FC, Fragment, useState } from "react";
 import styled from "styled-components";
 import { SelectVersions, Table, TD, TDInput, TDOutput, TH } from "./utils";
 import { versions } from "./versions";
 
+const versionNames = Object.keys(versions);
+
 type Result = {
+  out: ComplexArray,
   ifft_fft: number,
-  vs_fft01: number,
-  vs_bases: Record<string, number>,
+  vs: Record<string, number>,
 };
 
 type Results = Record<string, Result | Error>;
@@ -26,28 +28,36 @@ const dist = (
   return Math.sqrt(sum / n);
 }
 
+// empty-width field, making the left and right border appear as one fat border
+const SeparatorField = styled(TD)`
+  min-width: 0px;
+  max-width: 0px;
+  padding: 3px 0px;
+`;
+
+// empty-height field, making the top and bottom border appear as one fat border
+const SeparatorRowField = styled.td`
+  min-height: 0px;
+  max-height: 0px;
+  padding: 0;
+`;
+
 type TestFieldProps = {
+  irrelevant?: boolean,
+  equal?: boolean,
   warn?: boolean,
   error?: boolean,
-  noTopBorder?: boolean,
-  noRightBorder?: boolean,
-  noBottomBorder?: boolean,
-  noLeftBorder?: boolean,
-  padding?: string,
   right?: boolean,
   width?: string,
 };
 
 const TestField = styled(TD)<TestFieldProps>`
-  ${props => props.right ? "text-align: right;" : ""}
+  ${props => props.irrelevant ? "background: #ccc;" : ""}
+  ${props => props.equal ? "background: #aaf;" : ""}
   ${props => props.warn  ? "background: #ff4;" : ""}
   ${props => props.error ? "background: #f44;" : ""}
-  border-width:
-    ${props => props.noTopBorder ? "0" : "1px"}
-    ${props => props.noRightBorder ? "0" : "1px"}
-    ${props => props.noBottomBorder ? "0" : "1px"}
-    ${props => props.noLeftBorder ? "0" : "1px"};
-  padding: ${props => props.padding ?? "3px 5px"};
+  ${props => props.right ? "text-align: right;" : ""}
+  padding: 3px 5px;
   ${props => props.width !== undefined ? `
     min-width: ${props.width};
     max-width: ${props.width};
@@ -59,36 +69,27 @@ type DiffFieldProps = TestFieldProps & {
 }
 
 const DiffField: FC<DiffFieldProps> = ({children: diff, width, ...moreProps}) => (
-  <TestField right width={width ?? "6em"} warn={diff > 1e-14} {...moreProps}>
-    {(diff * 1e16).toFixed(3) + "e-16"}
+  <TestField
+    right width={width ?? "4.5em"}
+    equal={diff === 0} warn={diff > 1e-15} error={diff > 1e-14}
+    {...moreProps}
+  >
+    {diff === 0 ? "0" : diff.toExponential(3)}
   </TestField>
 );
-
-const ShowResult: FC<{result: Result | Error}> = ({result}) =>
-  result instanceof Error ? (
-    <TestField colSpan={2} error={true}>
-      {result.message}
-    </TestField>
-  ) : (
-    <>
-      <DiffField>{result.ifft_fft}</DiffField>
-      <DiffField>{result.vs_fft01}</DiffField>
-    </>
-  );
 
 const Legend: FC<{}> = () => (
   <>
     <p>Legend:</p>
     <ul>
-      <li><b>ifft*fft</b><br/>
-        Apply the FFT version to random input data and then the inverse FFT to
+      <li><b>ifft∘fft</b><br/>
+        Apply the FFT version to random input data and then the IFFT (inverse FFT) to
         the result.
         Compare the (scaled) result of the latter to the input data.
       </li>
-      <li><b>vs. fft01</b><br/>
-        Apply both the FFT version and fft01 (the reference version)
-        to random input data.
-        Compare the two results.
+      <li><b>vs. ...</b><br/>
+        Compare the results of applying two FFT versions to (the same) random
+        input data.
       </li>
     </ul>
     <p>
@@ -106,14 +107,7 @@ export const Tests: FC = () => {
   const [results, setResults] = useState({} as Results);
 
   async function runFFTs() {
-    if (!versions.fft01) {
-      setResults({})
-      return;
-    }
     const data = makeTestData(n);
-    const fft01 = versions.fft01(n);
-    const out01 = makeComplexArray(n);
-    fft01(data, out01, 1);
     let results: Results = {};
     for (const [name, func] of Object.entries(versions)) {
       if (!testVersions[name]) continue;
@@ -121,28 +115,48 @@ export const Tests: FC = () => {
         const fft = func(n);
         const out = makeComplexArray(n);
         fft(data, out, 1);
+        // // just to see how exceptions and diffs are handled:
+        // if (name==="fft06") setComplex(out, 0, timesScalar(getComplex(out, 0), 1.0001));
+        // if (name==="fft07") throw "fooooo";
         const invOut = makeComplexArray(n);
         fft(out, invOut, -1);
         const scale = 1/n;
-        const vs_bases: Record<string, number> = {};
         results[name] = {
-          name,
+          out,
           ifft_fft: dist(n,
             i => getComplex(data, i),
             i => timesScalar(getComplex(invOut, i), scale),
           ),
-          vs_fft01: dist(n,
-            i => getComplex(out, i),
-            i => getComplex(out01, i),
-          ) / Math.sqrt(n), // equivalent to scaling out and out01
-          vs_bases,
+          vs: {},
         };
       } catch (e) {
         results[name] = new Error(`Calculation failed: ${e}`);
       }
     }
+    for (const name1 of versionNames) {
+      const result1 = results[name1];
+      if (!result1 || result1 instanceof Error)
+        continue;
+      const out1 = result1.out;
+      for (const name2 of versionNames) {
+        if (name2 >= name1) continue;
+        const result2 = results[name2];
+        if (!result2 || result2 instanceof Error)
+          continue;
+        const out2 = result2.out;
+        const scale = Math.sqrt(1/n);
+        const distance = dist(n,
+          i => timesScalar(getComplex(out1, i), scale),
+          i => timesScalar(getComplex(out2, i), scale),
+        );
+        result1.vs[name2] = distance;
+        result2.vs[name1] = distance;
+      }
+    }
     setResults(results);
   }
+
+  const resultEntries = Object.entries(results);
 
   return (
     <>
@@ -171,22 +185,54 @@ export const Tests: FC = () => {
         </tbody>
       </table>
       <p>
-        Execute: <button onClick={runFFTs}>Run Tests</button>
+        Execute: {}
+        <button
+          onClick={runFFTs}
+          disabled={!Object.values(testVersions).some(value => value)}
+        >
+          Run Tests
+        </button>
       </p>
       <Table>
         <thead>
           <tr>
-            <TH>name</TH>
-            <TH>ifft*fft</TH>
-            <TH>vs. fft01</TH>
+            <TH/>
+            <SeparatorField/>
+            <TH>ifft∘fft</TH>
+            <SeparatorField/>
+            {Object.keys(results).map(name2 => (
+              <TH key={name2}>vs. {name2}</TH>
+            ))}
+          </tr>
+          <tr>
+            <SeparatorRowField colSpan={3 + resultEntries.length}/>
           </tr>
         </thead>
         <tbody>
-          {Object.entries(results).map(([name, result]) => (
+          {resultEntries.map(([name, result]) => (
             <Fragment key={name}>
               <tr>
-                <TD>{name}</TD>
-                <ShowResult result={result}/>
+                <TH>{name}</TH>
+                {result instanceof Error ? (
+                  <TestField colSpan={2 + resultEntries.length} error>
+                    {result.message}
+                  </TestField>
+                ) : (
+                  <>
+                    <SeparatorField/>
+                    <DiffField>{result.ifft_fft}</DiffField>
+                    <SeparatorField/>
+                    {resultEntries.map(([name2, result2]) => (
+                      <Fragment key={name2}>
+                        {
+                          name === name2           ? <TestField irrelevant/> :
+                          result2 instanceof Error ? <TestField error/> :
+                          <DiffField>{result.vs[name2]}</DiffField>
+                        }
+                      </Fragment>
+                    ))}
+                  </>
+                )}
               </tr>
             </Fragment>
           ))}
