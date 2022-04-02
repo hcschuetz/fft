def1762eea42808afa8ffd54aebaea7da0966a3e
Heribert Schütz <hs@webxcerpt.com>
2022-04-02 02:04:55 +0200

    split Benchmark component in smaller pieces

--- in-browser-test/src/Benchmark.tsx
+++ in-browser-test/src/Benchmark.tsx
@@ -1,16 +1,195 @@
-import { FC, Fragment, useRef, useState } from "react";
+import { FC, Fragment, MutableRefObject, useRef, useState } from "react";
 import styled from "styled-components";
 import { Table, TD, TDInput, TDOutput, TH } from "./utils";
 import { SelectVersions } from "./SelectVersions";
 import makeTestData from "./makeTestData";
-import { TestableFFT, useVersions } from "./VersionContext";
+import { TestableFFT, useVersions, VersionStates } from "./VersionContext";
 
 const sleep = (ms: number): Promise<undefined> =>
   new Promise(resolve => setTimeout(resolve, ms));
 
 const blockSizes = [100, 200, 500, 1000, 2000, 5000, 10000];
 
+type NArgs = {
+  log2n: number,
+  setLog2n(value: number): void,
+  n: number,
+};
+const ParameterRowN: FC<NArgs> = ({log2n, setLog2n, n}) => (
+  <tr>
+    <td>
+      <label htmlFor="nInput">data size:</label>
+    </td>
+    <TDInput>
+      <input id="nInput" type="range"
+        min={0} max={16}
+        value={log2n}
+        onChange={event => setLog2n(Number(event.target.value))}
+      />
+    </TDInput>
+    <TDOutput>{n}</TDOutput>
+  </tr>
+);
+
+type NBlocksArgs = {
+  nBlocks: number,
+  setNBlocks(value: number): void,
+};
+const ParameterRowNBlocks: FC<NBlocksArgs> = ({nBlocks, setNBlocks}) => (
+  <tr>
+    <td>
+      <label htmlFor="nBlocksInput">number of blocks:</label>
+    </td>
+    <TDInput>
+      <input id="nBlocksInput" type="range"
+        min={1} max={20}
+        value={nBlocks}
+        onChange={event => setNBlocks(Number(event.target.value))}
+      />
+    </TDInput>
+    <TDOutput>{nBlocks}</TDOutput>
+  </tr>
+);
+
+type BlockSizeArgs = {
+  blockSizeIdx: number,
+  setBlockSizeIdx(value: number): void,
+  blockSize: number,
+};
+const ParameterRowBlockSize: FC<BlockSizeArgs> = ({blockSizeIdx, setBlockSizeIdx, blockSize}) => (
+  <tr>
+    <td>
+      <label htmlFor="blockSizeInput">calls per block:</label>
+    </td>
+    <TDInput>
+      <input id="blockSizeInput" type="range"
+        min={0} max={blockSizes.length - 1}
+        value={blockSizeIdx}
+        onChange={event => setBlockSizeIdx(Number(event.target.value))}
+      />
+    </TDInput>
+    <TDOutput>{blockSize}</TDOutput>
+  </tr>
+);
+type PauseArgs = {
+  pause: number,
+  setPause(value: number): void,
+};
+const ParameterRowPause: FC<PauseArgs> = ({pause, setPause}) => (
+  <tr>
+    <td>
+      <label htmlFor="pauseInput">
+        pause before block:
+      </label>
+    </td>
+    <TDInput>
+      <input id="pauseInput" type="range"
+        min={0} max={60}
+        value={pause}
+        onChange={event => setPause(Number(event.target.value))}
+      />
+    </TDInput>
+    <TDOutput>{pause} s</TDOutput>
+  </tr>
+);
+
+const Parameters: FC<NArgs & NBlocksArgs & BlockSizeArgs & PauseArgs> = props => {
+  return (
+    <table style={{
+      margin: "1em 0",
+      borderCollapse: "collapse",
+      border: "1em 0",
+    }}>
+      <tbody>
+        <ParameterRowN {...props}/>
+        <ParameterRowNBlocks {...props}/>
+        <ParameterRowBlockSize {...props}/>
+        <ParameterRowPause {...props}/>
+      </tbody>
+    </table>
+  );
+};
+
 type benchmarkState = number | "" | "pause" | "run";
+
+type Results = Record<string, benchmarkState[]>;
+
+type ComputeArgs = {
+  thisCallCount: number,
+  callCount: MutableRefObject<number>,
+  versions: VersionStates,
+  testVersions: Record<string, boolean>,
+  nBlocks: number,
+  pause: number,
+  blockSize: number,
+  n: number,
+  setResults: (value: Results) => void,
+};
+
+async function compute({
+  thisCallCount, callCount,
+  versions, testVersions,
+  nBlocks, pause, blockSize, n,
+  setResults,
+}: ComputeArgs): Promise<void> {
+  function log(...args: any[]) {
+    console.log(`[${thisCallCount}|${callCount.current}]`, ...args);
+  }
+
+  function checkStop(tag: string) {
+    if (callCount.current !== thisCallCount) {
+      // eslint-disable-next-line no-throw-literal
+      throw `==== stopping (${tag})`;
+    }
+  }
+
+  try {
+    log("==== start");
+    const data = makeTestData(n);
+    const results: Results =
+      Object.fromEntries(
+        Object.keys(versions)
+        .filter((name) => testVersions[name])
+        .map((name) => [name, new Array(nBlocks).fill("") as benchmarkState[]])
+      );
+    for (const [name, version] of Object.entries(versions)) {
+      if (version.status !== "resolved" || !testVersions[name]) continue;
+      log("testing version", name);
+      const times = results[name];
+      const factory = version.value;
+      const fft: TestableFFT = factory(n);
+      data.forEach((v, i) => fft.setInput(i, v));
+      log("got func")
+      for (let i = 0; i < nBlocks; i++) {
+        checkStop("A");
+        if (pause > 0) {
+          times[i] = "pause";
+          setResults({...results});
+          log("begin pause", name, i);
+          await sleep(pause * 1e3);
+          log("end pause", name, i);
+          checkStop("B");
+        }
+        const start = performance.now();
+        log("run", name, i);
+        times[i] = "run";
+        setResults({...results});
+        await sleep(0);
+        for (let j = 0; j < blockSize; j++) {
+          fft.run(1);
+        }
+        checkStop("C");
+        const time = (performance.now() - start) * 1e-3 / blockSize;
+        times[i] = time;
+        setResults({...results});
+        await sleep(0);
+      }
+    }
+    log("==== completed");
+  } catch (e) {
+    log(e);
+  }
+}
 
 const BenchmarkField = styled(TD)`
   height: 2.8ex;
@@ -34,238 +213,87 @@ const visualizationModesBetter = [
   "🠔 better",
 ];
 
-const Benchmark: FC = () => {
-  const versions = useVersions();
-  const [testVersions, setTestVersions] = useState<Record<string, boolean>>({});
+const getRange = (resultValues: benchmarkState[][]) => {
+  const allTimes: number[] =
+    resultValues.flatMap(states =>
+      states.flatMap(state =>
+        typeof(state) === "number" ? [state] : []
+      )
+    );
+  return [Math.min(...allTimes), Math.max(...allTimes)];
+}
 
-  const [log2n, setLog2n] = useState(11);
-  const n = 1 << log2n;
-
-  const [nBlocks, setNBlocks] = useState(2);
-
-  const [blockSizeIdx, setBlockSizeIdx] = useState(4);
-  const blockSize = blockSizes[blockSizeIdx];
-
-  const [pause, setPause] = useState(0);
-  const [results, setResults] = useState<Record<string, benchmarkState[]>>({});
-
-  const initialRange = {fastest: Number.POSITIVE_INFINITY, slowest: 0};
-  const [range, setRange] = useState(initialRange);
-  const resetRange = () => setRange(initialRange);
-  const adaptRange = (time: number): void =>
-    setRange(({fastest, slowest}) => ({
-      fastest: Math.min(fastest, time),
-      slowest: Math.max(slowest, time),
-    }));
-
+const ResultsTable: FC<{results: Results}> = ({results}) => {
   const [visualizationModeIdx, setVisualizationModeIdx] = useState(2);
 
-  const callCount = useRef(0);
-
-  async function compute(thisCallCount: number): Promise<void> {
-    function log(...args: any[]) {
-      console.log(`[${thisCallCount}|${callCount.current}]`, ...args);
-    }
-
-    function checkStop(tag: string) {
-      if (callCount.current !== thisCallCount) {
-        // eslint-disable-next-line no-throw-literal
-        throw `==== stopping (${tag})`;
-      }
-    }
-
-    try {
-      log("==== start");
-      resetRange();
-      const data = makeTestData(n);
-      const results: Record<string, benchmarkState[]> =
-        Object.fromEntries(
-          Object.keys(versions)
-          .filter((name) => testVersions[name])
-          .map((name) => [name, new Array(nBlocks).fill("") as benchmarkState[]])
-        );
-      for (const [name, version] of Object.entries(versions)) {
-        if (version.status !== "resolved" || !testVersions[name]) continue;
-        log("testing version", name);
-        const times = results[name];
-        const factory = version.value;
-        const fft: TestableFFT = factory(n);
-        data.forEach((v, i) => fft.setInput(i, v));
-        log("got func")
-        for (let i = 0; i < nBlocks; i++) {
-          checkStop("A");
-          if (pause > 0) {
-            times[i] = "pause";
-            setResults({...results});
-            log("begin pause", name, i);
-            await sleep(pause * 1e3);
-            log("end pause", name, i);
-            checkStop("B");
-          }
-          const start = performance.now();
-          log("run", name, i);
-          times[i] = "run";
-          setResults({...results});
-          await sleep(0);
-          for (let j = 0; j < blockSize; j++) {
-            fft.run(1);
-          }
-          checkStop("C");
-          const time = (performance.now() - start) * 1e-3 / blockSize;
-          times[i] = time;
-          adaptRange(time);
-          setResults({...results});
-          await sleep(0);
-        }
-      }
-      log("==== completed");
-    } catch (e) {
-      log(e);
-    }
+  const resultValues = Object.values(results);
+  if (resultValues.length === 0) {
+    return null;
   }
-
-  const {fastest, slowest} = range;
+  const nBlocks = resultValues[0].length;
+  const [fastest, slowest] = getRange(resultValues);
 
   return (
-    <>
-      <p>Select the versions to benchmark:</p>
-      <SelectVersions selected={testVersions} setSelected={setTestVersions}/>
-      <p>Choose parameters:</p>
-      <table style={{
-        margin: "1em 0",
-        borderCollapse: "collapse",
-        border: "1em 0",
-      }}>
-        <tbody>
-          <tr>
-            <td>
-              <label htmlFor="nInput">data size:</label>
-            </td>
-            <TDInput>
-              <input id="nInput" type="range"
-                min={0} max={16}
-                value={log2n}
-                onChange={event => setLog2n(Number(event.target.value))}
-              />
-            </TDInput>
-            <TDOutput>{n}</TDOutput>
-          </tr>
-          <tr>
-            <td>
-              <label htmlFor="nBlocksInput">number of blocks:</label>
-            </td>
-            <TDInput>
-              <input id="nBlocksInput" type="range"
-                min={1} max={20}
-                value={nBlocks}
-                onChange={event => setNBlocks(Number(event.target.value))}
-              />
-            </TDInput>
-            <TDOutput>{nBlocks}</TDOutput>
-          </tr>
-          <tr>
-            <td>
-              <label htmlFor="blockSizeInput">calls per block:</label>
-            </td>
-            <TDInput>
-              <input id="blockSizeInput" type="range"
-                min={0} max={blockSizes.length - 1}
-                value={blockSizeIdx}
-                onChange={event => setBlockSizeIdx(Number(event.target.value))}
-              />
-            </TDInput>
-            <TDOutput>{blockSize}</TDOutput>
-          </tr>
-          <tr>
-            <td>
-              <label htmlFor="pauseInput">
-                pause before block:
-              </label>
-            </td>
-            <TDInput>
-              <input id="pauseInput" type="range"
-                min={0} max={60}
-                value={pause}
-                onChange={event => setPause(Number(event.target.value))}
-              />
-            </TDInput>
-            <TDOutput>{pause} s</TDOutput>
-          </tr>
-        </tbody>
-      </table>
-      <p>
-        Execute: {}
-        <button
-          onClick={() => compute(++callCount.current)}
-          disabled={!Object.values(testVersions).some(value => value)}
-        >
-          Run Benchmarks
-        </button>
-        {} <button onClick={() => ++callCount.current}>Stop Benchmarks</button>
-      </p>
-      {Object.keys(results).length > 0 && (
-        <Table>
-          <thead>
+    <Table>
+      <thead>
+        <tr>
+          <TH rowSpan={2}/>
+          <TH colSpan={nBlocks}>
+            time per call (in microseconds)
+          </TH>
+          <TD rowSpan={2} style={{verticalAlign: "top"}}>
+            <input style={{transform: "rotate(-90deg)", width: "3em", height: "3em"}}
+              type="range" min="0" max={visualizationModes.length - 1}
+              value={visualizationModeIdx}
+              onChange={event => setVisualizationModeIdx(Number(event.target.value))}
+            />
+            <div style={{display: "inline-block", width: "19em", textAlign: "center"}}>
+              <b>{visualizationModes[visualizationModeIdx]}</b>
+              <br/>
+              {visualizationModesBetter[visualizationModeIdx]}
+            </div>
+          </TD>
+        </tr>
+        <tr>
+          <TH colSpan={nBlocks} style={{background: "#eee"}}>
+            calls per second
+          </TH>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(results).map(([name, times]) => (
+          <Fragment key={name}>
             <tr>
-              <TH rowSpan={2}/>
-              <TH colSpan={Object.values(results)[0].length}>
-                time per call (in microseconds)
-              </TH>
-              <TD rowSpan={2} style={{verticalAlign: "top"}}>
-                <input style={{transform: "rotate(-90deg)", width: "3em", height: "3em"}}
-                  type="range" min="0" max={visualizationModes.length - 1}
-                  value={visualizationModeIdx}
-                  onChange={event => setVisualizationModeIdx(Number(event.target.value))}
-                />
-                <div style={{display: "inline-block", width: "19em", textAlign: "center"}}>
-                  <b>{visualizationModes[visualizationModeIdx]}</b>
-                  <br/>
-                  {visualizationModesBetter[visualizationModeIdx]}
-                </div>
-              </TD>
-            </tr>
-            <tr>
-              <TH colSpan={Object.values(results)[0].length} style={{background: "#eee"}}>
-                calls per second
+              <TH rowSpan={2} left>{name}</TH>
+              {times.map((time, j) => (
+                <BenchmarkField key={j}>
+                  {typeof(time) === "number" ? (time * 1e6).toFixed(1) : time}
+                </BenchmarkField>
+              ))}
+              <TH rowSpan={2} style={{verticalAlign: "middle"}}>
+                <svg viewBox="0 0 10 1" style={{width: "25em", height: "2.5em"}}>
+                  {times.map((time, blockNo) => typeof(time) === "number" && (
+                    <BlockVisualization
+                      key={blockNo}
+                      idx={visualizationModeIdx}
+                      blockNo={blockNo} nBlocks={nBlocks}
+                      time={time} fastest={fastest} slowest={slowest}
+                    />
+                  ))}
+                </svg>
               </TH>
             </tr>
-          </thead>
-          <tbody>
-            {Object.entries(results).map(([name, times]) => (
-              <Fragment key={name}>
-                <tr>
-                  <TH rowSpan={2} left>{name}</TH>
-                  {times.map((time, j) => (
-                    <BenchmarkField key={j}>
-                      {typeof(time) === "number" ? (time * 1e6).toFixed(1) : time}
-                    </BenchmarkField>
-                  ))}
-                  <TH rowSpan={2} style={{verticalAlign: "middle"}}>
-                    <svg viewBox="0 0 10 1" style={{width: "25em", height: "2.5em"}}>
-                      {times.map((time, blockNo) => typeof(time) === "number" && (
-                        <BlockVisualization
-                          key={blockNo}
-                          idx={visualizationModeIdx}
-                          blockNo={blockNo} nBlocks={times.length}
-                          time={time} fastest={fastest} slowest={slowest}
-                        />
-                      ))}
-                    </svg>
-                  </TH>
-                </tr>
-                <tr>
-                  {times.map((time, j) => (
-                    <BenchmarkFieldCalls key={j}>
-                      {typeof(time) === "number" ? (1/time).toFixed() : ""}
-                    </BenchmarkFieldCalls>
-                  ))}
-                </tr>
-              </Fragment>
-          ))}
-          </tbody>
-        </Table>
-      )}
-    </>
+            <tr>
+              {times.map((time, j) => (
+                <BenchmarkFieldCalls key={j}>
+                  {typeof(time) === "number" ? (1/time).toFixed() : ""}
+                </BenchmarkFieldCalls>
+              ))}
+            </tr>
+          </Fragment>
+        ))}
+      </tbody>
+    </Table>
   );
 };
 
@@ -312,5 +340,53 @@ const BlockVisualization: FC<{
     default: return null;
   }
 }
+
+const Benchmark: FC = () => {
+  const versions = useVersions();
+  const [testVersions, setTestVersions] = useState<Record<string, boolean>>({});
+
+  const [log2n, setLog2n] = useState(11);
+  const n = 1 << log2n;
+
+  const [nBlocks, setNBlocks] = useState(2);
+
+  const [blockSizeIdx, setBlockSizeIdx] = useState(4);
+  const blockSize = blockSizes[blockSizeIdx];
+
+  const [pause, setPause] = useState(0);
+  const [results, setResults] = useState<Results>({});
+
+  const callCount = useRef(0);
+
+  return (
+    <>
+      <p>Select the versions to benchmark:</p>
+      <SelectVersions selected={testVersions} setSelected={setTestVersions}/>
+      <p>Choose parameters:</p>
+      <Parameters {...{
+        log2n, setLog2n, n,
+        nBlocks, setNBlocks,
+        blockSizeIdx, setBlockSizeIdx, blockSize,
+        pause, setPause,        
+      }}/>
+      <p>
+        Execute: {}
+        <button
+          onClick={() => compute({
+            thisCallCount: ++callCount.current, callCount,
+            versions, testVersions,
+            nBlocks, pause, blockSize, n,
+            setResults,
+          })}
+          disabled={!Object.values(testVersions).some(value => value)}
+        >
+          Run Benchmarks
+        </button>
+        {} <button onClick={() => ++callCount.current}>Stop Benchmarks</button>
+      </p>
+      <ResultsTable {...{results}}/>
+    </>
+  );
+};
 
 export default Benchmark;
